a2e0fe9ac2a52c3906d3c5c2bbd13e5f01ce69fd
Luke Wood <lewoodwebdev@gmail.com>
2017-05-20 21:48:01 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Voting App
+### Dynamic Web Application for FreeCodeCamp
+
+## User Stories
+
+##### As an authenticated user, I can:
+- keep my polls and come back later to access them
+- share my polls with my friends
+- see the aggregate results of my polls
+- delete polls that I decide I don't want anymore
+- create a poll with any number of possible items
+- create a new option if I don't like the options on a poll
+
+##### As an authenticated or unauthenticated user, I can:
+- see the results of polls in chart form
+
+### License
+
+MIT License.
---
+++ pseudo.js
@@ -0,0 +1,38 @@
+/*
+
+-create FB auth module in Voting App
+
+  -set up passport-facebook
+    -set up a branch to test feature 
+    -create app in FB dev
+      -get FACEBOOK_APP_ID
+      -get FACEBOOK_APP_SECRET
+    -create default route in branch to check
+      -put angular logic in to check auth
+    -create a test to check user login success
+      -copy all tutorial files into branch
+      -create a unit test file, log(result)
+      
+      
+  
+  -set up component (in another branch)
+  
+    -set up templates for component screens
+    
+      -set up pre-login template
+         -set up route for login
+         -create html file for pre-login
+           -username input field
+           -password input field
+           -login submit button
+         
+      -set up post-login template
+        -set up user info in right top corner
+          -"Hello, {{ username }}"
+          -logout button next to greeting
+      -
+
+
+
+
+*/
